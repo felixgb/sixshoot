8558d7c605ceb245015eba23a78ec541bcb5ba3d
felix bowman <felixgbowman@gmail.com>
2019-10-12 11:56:51 +0100

    use glfw instead of sdl, keys work better

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-#![feature(clamp)]
-extern crate nalgebra;
 extern crate gl;
-extern crate sdl2;
+extern crate glfw;
+extern crate nalgebra;
 
 pub mod render;
 pub mod obj;
@@ -10,32 +9,30 @@ pub mod uniform;
 pub mod model;
 
 use nalgebra::*;
-use sdl2::keyboard::Keycode;
 use std::time::SystemTime;
+use glfw::*;
 
-fn create_window(video_subsystem: &sdl2::VideoSubsystem) -> sdl2::video::Window {
-    video_subsystem
-        .window("HA HA", 1920, 1080)
-        .opengl()
-        .resizable()
-        .build()
-        .unwrap()
-}
+const HEIGHT: u32 = 1080;
+const WIDTH: u32 = 1920;
 
 fn main() {
-    let sdl = sdl2::init().unwrap();
-    let video_subsystem = sdl.video().unwrap();
+    let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
 
-    let gl_attr = video_subsystem.gl_attr();
-    gl_attr.set_context_profile(sdl2::video::GLProfile::Core);
-    gl_attr.set_context_version(4, 5);
+    let (mut window, events) = glfw.create_window(
+        WIDTH,
+        HEIGHT,
+        "hello i am window",
+        glfw::WindowMode::Windowed
+    ).expect("failed to create window!");
 
-    let window = create_window(&video_subsystem);
-    sdl.mouse().set_relative_mouse_mode(true);
+    window.set_framebuffer_size_polling(true);
+    window.set_cursor_mode(CursorMode::Disabled);
+    window.set_cursor_pos_polling(true);
+    window.set_key_polling(true);
+    window.make_current();
 
     let _gl =
-        gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void);
-    let _gl_context = window.gl_create_context().unwrap();
+        gl::load_with(|s| window.get_proc_address(s) as *const std::os::raw::c_void);
 
     unsafe {
         gl::Viewport(0, 0, 1920, 1080);
@@ -50,12 +47,11 @@ fn main() {
     // let vertices = obj::read_lines().unwrap().compute_faces();
 
     let cubes = vec![
-        model::Model::test_cube_model(Point3::new(0.0, 0.0, -3.0)),
+        model::Model::test_cube_model(Point3::new(-3.0, 0.0, 0.0)),
         model::Model::test_cube_model(Point3::new(0.0, 0.0, 0.0)),
-        model::Model::test_cube_model(Point3::new(0.0, 0.0, 3.0)),
+        model::Model::test_cube_model(Point3::new(3.0, 0.0, 0.0)),
     ];
 
-    let mut events = sdl.event_pump().unwrap();
     let mut iter = 0.0;
 
     let transform = uniform::Uniform::get_uniform_location(program.id, "transform").unwrap();
@@ -64,40 +60,52 @@ fn main() {
         gl::PolygonMode(gl::FRONT_AND_BACK, gl::LINE);
     }
 
-    let projection = Perspective3::new(16.0 / 9.0, 3.14 / 2.0, 1.0, 1000.0);
+    let projection = Perspective3::new(16.0 / 9.0, 3.14 / 2.0, 0.1, 1000.0);
 
-    let mut camera_pos: Vector3<f32> = Vector3::new(0.0, 0.0, 10.0);
-    let mut camera_front: Vector3<f32> = Vector3::new(0.0, 0.0, -1.0);
+    let mut camera_pos: Vector3<f32> = Vector3::new(0.0, 0.0, -10.0);
+    let mut camera_front: Vector3<f32> = Vector3::new(0.0, 0.0, 1.0);
     let camera_up = Vector3::y();
 
     let mut delta_time;
     let mut last_frame = 0.0;
     let start = SystemTime::now();
 
-    let mut last_x = 0;
-    let mut last_y = 0;
+    let mut last_x = 1920.0 / 2.0;
+    let mut last_y = 1080.0 / 2.0;
 
-    let mut yaw = 180.0;
+    let mut yaw = 0.0;
     let mut pitch = 0.0;
 
     let sensitivity = 0.5;
-    'main: loop {
+    let mut forward = false;
+    let mut right = false;
+    let mut backward = false;
+    let mut left = false;
+    while !window.should_close() {
         let current_time = SystemTime::now().duration_since(start).unwrap().as_millis() as f32;
         delta_time = current_time - last_frame;
         last_frame = current_time;
         let camera_speed = 0.005 * delta_time;
 
-        for event in events.poll_iter() {
+        glfw.poll_events();
+        for (_, event) in glfw::flush_messages(&events) {
+            println!("{:?}", event);
             match event {
-                sdl2::event::Event::Quit {..} => break 'main,
-                sdl2::event::Event::MouseMotion { x, y, .. } => {
-                    let x_offset = (x - last_x) as f32 * sensitivity;
-                    let y_offset = (last_y - y) as f32 * sensitivity;
+                glfw::WindowEvent::CursorPos(x, y) => {
+                    let x_offset = (x - last_x) as f32;
+                    let y_offset = (last_y - y) as f32;
+
                     last_x = x;
                     last_y = y;
-                    yaw += x_offset;
-                    pitch += y_offset;
-                    pitch = pitch.clamp(-89.0, 89.0);
+
+                    yaw += x_offset * sensitivity;
+                    pitch += y_offset * sensitivity;
+                    if pitch > 89.0 {
+                        pitch = 89.0;
+                    }
+                    if pitch < -89.0 {
+                        pitch = -89.0;
+                    }
 
                     let front = Vector3::new(
                         yaw.to_radians().cos() * pitch.to_radians().cos(),
@@ -106,24 +114,51 @@ fn main() {
                     );
                     camera_front = front.normalize();
                 }
-                sdl2::event::Event::KeyDown { keycode: Some(key), .. } => match key {
-                    Keycode::Q => break 'main,
-                    Keycode::W => {
-                        camera_pos += camera_speed * camera_front;
+                glfw::WindowEvent::Key(key, _, action, _) => match key {
+                    Key::Q => window.set_should_close(true),
+                    Key::W => {
+                        match action {
+                            Action::Press => forward = true,
+                            Action::Release => forward = false,
+                            _ => { }
+                        }
                     },
-                    Keycode::A => {
-                        camera_pos -= camera_front.cross(&camera_up).normalize() * camera_speed;
+                    Key::A => {
+                        match action {
+                            Action::Press => left = true,
+                            Action::Release => left = false,
+                            _ => { }
+                        }
                     },
-                    Keycode::S => {
-                        camera_pos -= camera_speed * camera_front;
+                    Key::S => {
+                        match action {
+                            Action::Press => backward = true,
+                            Action::Release => backward = false,
+                            _ => { }
+                        }
                     },
-                    Keycode::D => {
-                        camera_pos += camera_front.cross(&camera_up).normalize() * camera_speed;
+                    Key::D => {
+                        match action {
+                            Action::Press => right = true,
+                            Action::Release => right = false,
+                            _ => { }
+                        }
                     },
                     _ => { },
                 },
                 _ => { }
             }
+        }
+
+        if forward {
+            camera_pos += camera_speed * camera_front;
+        } else if backward {
+            camera_pos -= camera_speed * camera_front;
+        }
+        if right {
+            camera_pos += camera_front.cross(&camera_up).normalize() * camera_speed;
+        } else if left {
+            camera_pos -= camera_front.cross(&camera_up).normalize() * camera_speed;
         }
 
         let p = Point3::from(camera_pos);
@@ -141,9 +176,9 @@ fn main() {
             cube.draw();
         }
 
-        window.gl_swap_window();
         iter = iter + 0.05;
 
+        window.swap_buffers();
         unsafe {
             gl::Clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT);
         }
